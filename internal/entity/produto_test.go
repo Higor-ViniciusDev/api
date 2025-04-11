d3b335b0d0e7fba09f5a7a5195e02421d4d21042
Higor <higor.vinicius331@hotmail.com>
2025-04-11 09:57:06 -0300

    Finalizando teste produto

--- internal/entity/produto_test.go
+++ internal/entity/produto_test.go
@@ -10,5 +10,6 @@ func TestNewProduto(t *testing.T) {
 	prod, err := NovoProduto("caixa papelão 12", 64.64)
 
 	assert.Nil(t, err, "Não pode haver error na criacao")
-	assert.Empty(t, prod.Nome, "Nome não pode ser vazio")
+	assert.NotEmpty(t, prod.Nome, "Nome não pode ser vazio")
+	assert.NotEmpty(t, prod.ID, "ID não pode ser vazio")
 }
--- internal/infra/database/product_db_test.go
+++ internal/infra/database/product_db_test.go
@@ -35,7 +35,7 @@ func TestNovoProdutoBanco(t *testing.T) {
 }
 
 func TestProcuraTodos(t *testing.T) {
-	db, err := gorm.Open(sqlite.Open("file:memory"), &gorm.Config{})
+	db, err := gorm.Open(sqlite.Open("file::memory:?cache=shared"), &gorm.Config{})
 
 	if err != nil {
 		t.Error(err)
@@ -57,4 +57,83 @@ func TestProcuraTodos(t *testing.T) {
 	produtos, err = newProductDb.ProcuraTodos(10, 2, "ASC")
 	assert.Nil(t, err)
 	assert.Equal(t, "Produto 20", produtos[9].Nome)
+}
+
+func TestProcuraPorID(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:?cache=shared"), &gorm.Config{})
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	db.AutoMigrate(&entity.Produto{})
+	produto, err := entity.NovoProduto("Produto", rand.Float64()*100)
+	assert.Nil(t, err)
+	newProductDb := NovoProdutoDB(db)
+
+	err = newProductDb.CreateProdutoDB(produto)
+	assert.Nil(t, err)
+
+	novoProduto, err := newProductDb.ProcuraPorID(produto.ID.String())
+	assert.Nil(t, err)
+	assert.Equal(t, produto.ID.String(), novoProduto.ID.String())
+}
+
+func TestAlteraProduto(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:?cache=shared"), &gorm.Config{})
+
+	if err != nil {
+		t.Error(err)
+	}
+	//Cria as tabela
+	db.AutoMigrate(&entity.Produto{})
+	produto, err := entity.NovoProduto("Produto", rand.Float64()*100)
+	assert.Nil(t, err)
+
+	//Cria conexão
+	newProductDb := NovoProdutoDB(db)
+	db.Create(&produto)
+
+	//Alterar produto
+	produto.Nome = "NOVO PRODUTO"
+	produto.Preco = 100.0
+
+	//chama a função de alteração
+	err = newProductDb.AlteraProduto(produto)
+	assert.Nil(t, err, "Não pode haver erro na alteração")
+
+	var produtoAlterado entity.Produto
+	err = db.First(&produtoAlterado, "id = ?", produto.ID).Error
+	//Validando de tudo deu certo
+	assert.Nil(t, err)
+	assert.Equal(t, "NOVO PRODUTO", produtoAlterado.Nome)
+	assert.Equal(t, 100.0, produtoAlterado.Preco)
+
+	//Validar erro se passar uma entidade não criada
+	novoProd, _ := entity.NovoProduto("Produto", rand.Float64()*100)
+	err = newProductDb.AlteraProduto(novoProd)
+	assert.NotNil(t, err)
+}
+
+func TestDeletaProduto(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("file::memory:?cache=shared"), &gorm.Config{})
+
+	if err != nil {
+		t.Error(err)
+	}
+	//Cria as tabela
+	db.AutoMigrate(&entity.Produto{})
+	produto, err := entity.NovoProduto("Produto", rand.Float64()*100)
+	assert.Nil(t, err)
+
+	//Cria conexão
+	newProductDb := NovoProdutoDB(db)
+	db.Create(&produto)
+
+	newProductDb.Apagar(produto.ID.String())
+
+	var produtoDeletado entity.Produto
+	err = db.First(&produtoDeletado, "id = ?", produto.ID).Error
+	assert.NotNil(t, err)
+	assert.Empty(t, produtoDeletado)
 }
